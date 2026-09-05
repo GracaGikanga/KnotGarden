65ab8a95a8d52733b9ce74beee51df314216e6b7
GracaGikanga <gracagikanga@gmail.com>
2026-09-06 00:48:49 +0300

    “changes”

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //Button A
-<button id="btnA">Chat on WhatsApp</button>
 
   document.getElementById('btnA').addEventListener('click', function() {
     // 1. Phone number must include country code without '+', spaces, or dashes
