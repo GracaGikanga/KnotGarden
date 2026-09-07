58aaee6203322a8a1088a42bd2aa44334f745b58
GracaGikanga <gracagikanga@gmail.com>
2026-09-07 13:29:48 +0300

    “changes”

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = 'tel:+254704856247';
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('btnC').addEventListener('click', function () {
+        var phone = this.getAttribute('data-phone');
+        window.location.href = 'sms:' + phone;
+    });
+});
