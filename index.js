5ea1210e38b7de7cdcc7946f44e3ffb9167a8ab0
GracaGikanga <gracagikanga@gmail.com>
2026-09-07 13:08:54 +0300

    “changes”

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-document.getElementById('btnB').addEventListener('click', function() {
-        window.location.href = 'tel:254704856247';
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('btnB').addEventListener('click', function () {
+        window.location.href = 'tel:+254700000000';
     });
+});
